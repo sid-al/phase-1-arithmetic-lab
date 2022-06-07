7772cf7b83ba7cade8168bd3665ad78ea610657a
sid-al <sendplusx@gmail.com>
2022-06-07 05:05:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// function multiply(num1,num2){
+//     return num1 * num2;
+// }
+
+// multiply(31,2);
+
+const num1 = 31;
+const num2 = 2;
+const  multiply  = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4;
+ 
+
+const numSet = (10,12,16,18,20);
+const max = math.max(10,12,16,18,20);
+
