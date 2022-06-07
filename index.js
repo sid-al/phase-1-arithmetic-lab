8c60d69bdec6648a3da3c729dc88392baa80b7f7
sid-al <sendplusx@gmail.com>
2022-06-07 07:11:16 +0300

    commit tree ec10bbbc399742f15e17926ef3a07f8827d39478

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const num1 = 31;
 const num2 = 2;
 const  multiply  = num1 * num2;
 
-let random = Math.floor(Math.random() * 10) + 1;
+let random = Math.floor(Math.random() * 20) + 1;
 
 const num3 = 12;
 const num4 = 8;
@@ -18,5 +18,5 @@ const mod = num3 % num4;
  
 
 const numSet = (10,12,16,18,20);
-const max = Math.max(10,12,16,18,20);
+const max = Math.max(numSet);
 
